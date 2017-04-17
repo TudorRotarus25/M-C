bc982e2b859c04a490d256a9f5df8e4241c84ee0
Tudor Rotarus <tudor.rotarus@sportpursuit.co.uk>
2017-04-17 23:53:56 +0100

    Landing page - no images

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 $(document).ready(function () {
-    $("#contact-us-anchor").on("click", function (e) {
-        $(".modal").toggle("fold", {}, 500);
-        e.preventDefault();
-    });
-    
-    $(".wrapper").on("click", function (e) {
-        $(".modal").hide("fold", {}, 500);
-    });
+    // $("#contact-us-anchor").on("click", function (e) {
+    //     $(".modal").toggle("fold", {}, 500);
+    //     e.preventDefault();
+    // });
+    //
+    // $(".wrapper").on("click", function (e) {
+    //     $(".modal").hide("fold", {}, 500);
+    // });
+    //
+    // $(document).tooltip({
+    //     position: {
+    //         my: "center bottom-20",
+    //         at: "center top",
+    //         using: function( position, feedback ) {
+    //             $( this ).css( position );
+    //             $( "<div>" )
+    //                 .addClass( "arrow" )
+    //                 .addClass( feedback.vertical )
+    //                 .addClass( feedback.horizontal )
+    //                 .appendTo( this );
+    //         }
+    //     }
+    // });
    
 });
 
