0b37cb6dcffec814c296dc9a63a45c9df769871b
Tudor Rotarus <tudor.rotarus@sportpursuit.co.uk>
2017-04-22 17:53:15 +0100

    Added scroll things and Google Analytics

--- js/main.js
+++ js/main.js
@@ -1,28 +1,48 @@
+function Scroll() { }
+
+Scroll.prototype = {
+    initialize: function () {
+        this.workAnchor = $('#work-anchor');
+        this.aboutAnchor = $('#about-anchor');
+        this.workSection = $('#work');
+        this.aboutSection = $('#about');
+        this.initialiseListeners();
+    },
+    initialiseListeners: function () {
+        var scope = this;
+
+        if (scope.workSection) {
+            this.workAnchor.on('click', function () {
+                scope.scrollTo(scope.workSection);
+            });
+            this.aboutAnchor.on('click', function () {
+                scope.scrollTo(scope.aboutSection);
+            });
+
+            $(window).scroll(function () {
+                var workTop = scope.workSection.offset().top - 200,
+                    aboutTop = scope.aboutSection.offset().top - 200,
+                    windowScroll = $(this).scrollTop();
+                if (windowScroll >= workTop && windowScroll < aboutTop) {
+                    scope.workAnchor.addClass('selected');
+                    scope.aboutAnchor.removeClass('selected');
+                } else if (windowScroll >= aboutTop) {
+                    scope.workAnchor.removeClass('selected');
+                    scope.aboutAnchor.addClass('selected');
+                }
+            });
+        }
+    },
+    scrollTo: function (element) {
+        $('html, body').animate({
+            scrollTop:  element.offset().top - 160
+        }, 1000);
+    }
+};
+
 $(document).ready(function () {
-    // $("#contact-us-anchor").on("click", function (e) {
-    //     $(".modal").toggle("fold", {}, 500);
-    //     e.preventDefault();
-    // });
-    //
-    // $(".wrapper").on("click", function (e) {
-    //     $(".modal").hide("fold", {}, 500);
-    // });
-    //
-    // $(document).tooltip({
-    //     position: {
-    //         my: "center bottom-20",
-    //         at: "center top",
-    //         using: function( position, feedback ) {
-    //             $( this ).css( position );
-    //             $( "<div>" )
-    //                 .addClass( "arrow" )
-    //                 .addClass( feedback.vertical )
-    //                 .addClass( feedback.horizontal )
-    //                 .appendTo( this );
-    //         }
-    //     }
-    // });
-   
+    var scroll = new Scroll();
+    scroll.initialize();
 });
 
 
